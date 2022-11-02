86f8c8ad9abbe0398d7df56f3fafb298b43015c5
Morgan Kelly <mkelly5380@gmail.com>
2022-11-02 08:53:40 -0500

    getting one product in product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
   //finding all categories in Category 
   //including products in the categories 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all products 
+  // find all includes the category and tag
   const productData = await Product.findAll({
     include: {
       model: Category,
@@ -17,9 +15,18 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  const productData = await Product.findByPk(req.params.id,{ 
+    include: 
+    ({
+       model: Category 
+      },
+       {model: Tag 
+      }
+      ) 
+    });
+    return res.json(productData);
 });
 
 // create new product
