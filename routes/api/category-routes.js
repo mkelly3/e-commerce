f84cb9ec046c55af873215a70646c2934c8d0fe8
Morgan Kelly <mkelly5380@gmail.com>
2022-11-02 09:13:19 -0500

    used the try method in categories to fix an error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,24 +2,37 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
-  //finding all categories in Category 
-  //including products in the categories 
-  const categoryData = await Category.findAll({ include: Product });
-  return res.json(categoryData);
-  
+
+  try {
+    const categoryData = await Category.findAll({ include: Product });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // includes its associated product 
-  const categoryData = await Category.findByPk(req.params.i, {include: Product});
-  return res.json(categoryData);
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {include: Product });
+      // include: [{ model: Product, through: Category, as: 'product_id' }]
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category by this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
-  const categoryData = await Category.create(req.body);
-  return res.json(categoryData)
+  
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
